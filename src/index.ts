feb7f03a56ac65635cf1830c73a6f4c5161bebec
Preston Neal <preston.neal@edf-re.com>
2023-03-14 18:01:15 -0700

    read from file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
+import fs from 'node:fs'
 import { argv } from 'node:process';
 console.log(argv[2]);
+try {
+    const data = fs.readFileSync(argv[2], 'utf-8')
+    console.log(data)
+} catch (error) {
+    console.error(error)
+}
 
