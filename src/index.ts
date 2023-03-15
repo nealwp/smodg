07e9b026f177bd351b515a871fa9d9003481c92e
Preston Neal <preston.neal@edf-re.com>
2023-03-14 20:22:48 -0700

    output is logged to console

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 import fs from 'node:fs'
 import { argv } from 'node:process';
-console.log(argv[2]);
+import { generateModel } from './parser';
+
 try {
-    const data = fs.readFileSync(argv[2], 'utf-8')
+    const sourceCode = fs.readFileSync(argv[2], 'utf-8')
+    const data = generateModel(sourceCode)
     console.log(data)
 } catch (error) {
     console.error(error)
