ed841ef16d5ed46b596c58d5152c2d9c6965764c
Preston Neal <preston.neal@edf-re.com>
2023-03-14 19:40:40 -0700

    reformat

--- tests/parser.spec.ts
+++ tests/parser.spec.ts
@@ -5,22 +5,35 @@ describe('parser', () => {
     describe('readTokensFromSource', () => {
         test('should return tokenized file content', () => {
             const fileContent = `export type SmallTest = {\nname: string;\n}`
-            const expectedOutput = [{kind: "ExportKeyword", text: "export"}, {kind: "TypeKeyword", text: "type"}, {kind: "Identifier", text: "SmallTest"}, {kind: "FirstAssignment", text: "="}, {kind: "FirstPunctuation", text: "{"}, {kind: "Identifier", text: "name"}, {kind: "ColonToken", text: ":"}, {kind: "StringKeyword", text: "string"}, {kind: "SemicolonToken", text: ";"}, {kind: "CloseBraceToken", text: "}"}]
+            
+            const expectedOutput = [
+                { kind: "ExportKeyword", text: "export" }, 
+                { kind: "TypeKeyword", text: "type" }, 
+                { kind: "Identifier", text: "SmallTest" }, 
+                { kind: "FirstAssignment", text: "=" }, 
+                { kind: "FirstPunctuation", text: "{" }, 
+                { kind: "Identifier", text: "name" }, 
+                { kind: "ColonToken", text: ":" }, 
+                { kind: "StringKeyword", text: "string" }, 
+                { kind: "SemicolonToken", text: ";" }, 
+                { kind: "CloseBraceToken", text: "}" }
+            ]
+
             const tokens = readTokensFromSource(fileContent)
             expect(tokens).toEqual(expectedOutput)
         })
     })
 
-    describe('generateModel',  () => {
+    describe('generateModel', () => {
         test('should return a sequelize model', () => {
             const fileContent = `export type SmallTest = {
                 name: string;
             }`
-    
+
             const expectedOutput = `@Column({ field: 'name', type: Sequelize.STRING })\nname!: string`
-    
+
             const result = generateModel(fileContent)
-    
+
             expect(result).toEqual(expectedOutput)
         })
     })
