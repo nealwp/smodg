1265bc63767d9d671a41820685283923ed94dbf9
Preston Neal <preston.neal@edf-re.com>
2023-03-15 20:16:00 -0700

    column definition and decorators write to file

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,19 @@
 
 import fs from 'node:fs'
 import { argv } from 'node:process';
-import { generateModel } from './parser';
+import { generateColumnDefinition, generateModel } from './parser';
 
 try {
     const sourceCode = fs.readFileSync(argv[2], 'utf-8')
-    const data = generateModel(sourceCode)
+    const columnDecorators = generateModel(sourceCode)
+    const columnDefintion = generateColumnDefinition(sourceCode)
     
     if (!fs.existsSync('./src/models')) {
         fs.mkdirSync('./src/models')
     }
 
     try {
-        fs.writeFileSync('./src/models/test.model.ts', data)
+        fs.writeFileSync('./src/models/test.model.ts', `${columnDefintion}\n${columnDecorators}`)
     } catch (error) {
         console.error(error)
     }
