dcbe789ed6c9969d5bbcd93593edfef2cf468f09
Preston Neal <mrprestonneal@gmail.com>
2023-05-10 21:03:39 -0700

    printing help works

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,71 @@
-#!/usr/bin/env node
-
+#!/usr/bin/env node 
 import fs from 'node:fs'
-import { argv } from 'node:process';
 import { generateModelInputs } from './parser';
 import { modelTemplate } from './templates/model.smodg';
 import { kebabCase } from './formatters';
+import minimist from 'minimist';
 
-try {
-    const sourceCode = fs.readFileSync(argv[2], 'utf-8')
-    const modelInputs = generateModelInputs(sourceCode)
-    
-    const model = modelTemplate(modelInputs)
+const main = () => {
+    const args = minimist(process.argv.slice(2)) 
+    const filePath = args._[0] 
 
-    if (!fs.existsSync('./src/models')) {
-        fs.mkdirSync('./src/models')
+    console.log(args)
+
+    if(args.h || args.help){
+        printHelp()
+        return
     }
 
+    if(args.v || args.version){
+        printVersion()
+        return
+    }
+        
+}
+
+const generateModel = (filePath: string) => {
     try {
-        fs.writeFileSync(`./src/models/${kebabCase(modelInputs.modelName)}.model.ts`, model)
+        const sourceCode = fs.readFileSync(filePath, 'utf-8')
+        const modelInputs = generateModelInputs(sourceCode)
+
+        const model = modelTemplate(modelInputs)
+
+        if (!fs.existsSync('./src/models')) {
+            fs.mkdirSync('./src/models')
+        }
+
+        try {
+            fs.writeFileSync(`./src/models/${kebabCase(modelInputs.modelName)}.model.ts`, model)
+        } catch (error) {
+            console.error(error)
+        }
+
     } catch (error) {
         console.error(error)
     }
 
-} catch (error) {
-    console.error(error)
 }
+
+const printHelp = () => {
+    const helpText = `
+=================================
+    Sequelize Model Generator    
+=================================
+
+Usage:
+
+    smodg [options] <filepath> 
+
+Options:
+
+    -h, --help        show help
+    -m, --migration   create an Umzug migration. default false
+    -o, --outputDir   model output directory. default ./src/models
+    -s, --schema      specify a schema. default is none/public 
+    -v, --version     print installed version
+` 
+
+    console.log(helpText)
+}
+
+main()
