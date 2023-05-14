19ba5502c776836b2f9301aa15d7b95eaf97370e
nealwp <mrprestonneal@gmail.com>
2023-05-13 22:14:18 -0700

    add migration generation

--- src/index.ts
+++ src/index.ts
@@ -61,7 +61,7 @@ const main = () => {
             }
 
             const migration = migrationTemplate(migrationInputs)
-            //writeMigrationToFile(migration)
+            writeMigrationToFile(migration, {modelName: modelInputs.modelName})
         }
     } catch(error) {
         console.error(error)
@@ -80,6 +80,32 @@ const writeModelToFile = (model: string, options: {outputDir: string, modelName:
     } catch (error) {
         console.error(error)
     }
+}
+
+const writeMigrationToFile = (migration: string, options: {modelName: string}) => {
+     
+    if (!fs.existsSync(`./src/migrations`)) {
+        fs.mkdirSync(`./src/migrations`)
+    }
+
+    try {
+        fs.writeFileSync(`./src/migrations/${dateFormatString()}-Create-Table-${options.modelName}.ts`)
+    } catch(error) {
+        console.error(error)
+    }
+}
+
+
+const dateFormatString = () => {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    const hours = String(currentDate.getHours()).padStart(2, '0');
+    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+    const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+
+    return `${year}.${month}.${day}T${hours}.${minutes}.${seconds}`;
 }
 
 const printHelp = () => {
